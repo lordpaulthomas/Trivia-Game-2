19a39a277bbcaec5b210ea69b2f5e1be5df115f2
Paul Thomas <traitorpt@gmail.com>
2019-09-09 23:00:56 -0700

    added giph for each ojbect in question array

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,6 +1,5 @@
 $('#start').on('click', function(){
     $('#start').remove();
-    console.log(game.loadQuestion)
     game.loadQuestion();
 })
 
@@ -13,51 +12,61 @@ const questions = [
       question: "Who becomes the Minister for Magic When Cornelius Fudge resigns?",
       answers: ["Rufus Scrimgeour","Professor Umbridge","Professor Snape","Mr Crouch"],
       correctAnswer: "Rufus Scrimgeour",
+      image: "<img src='https://media.giphy.com/media/XduXaSnqatLgI/giphy.gif'>"
   },
   {
       question: "How many brothers and sisters does Ron have?",
       answers: ["2", "6", "5", "8" ],
-      correctAnswer: '6',
+      correctAnswer: "6",
+      image: "<img src='https://media.giphy.com/media/SLanXKeqqebci4LqX8/giphy.gif'>",
   },
   {
       question: "Who was Ginny Weasley's first boyfriend?",
       answers: ["Dean Thomas", "Harry Potter","Michael Corner", "Draco Malfoy"],
-      correctAnswer: 'Michael Corner'
+      correctAnswer: 'Michael Corner',
+      image: "<img src='https://media.giphy.com/media/eWM99f3s7Odd6/giphy.gif>"
   },
   {
       question: "What profession are Hermoine Granger's Muggle parents?",
       answers: ['Chefs', 'Dentists', 'Opticians', 'Artists'],
-      correctAnswer: 'Dentists'
+      correctAnswer: 'Dentists',
+      image: "<img src='https://media.giphy.com/media/155lQxHOx9Bni/giphy.gif'>"
   },
   {
       question: "What was Fred and George's joke shop's name?",
       answers: ["Wizard's Tricks", "Weasley's Wizarding Gadets", "Weasley's Wizard Wheezes", 'Cloaks and Jokes'],
-      correctAnswer: "Weasley's Wizard Wheezes"
+      correctAnswer: "Weasley's Wizard Wheezes",
+      image: "<img src='https://media.giphy.com/media/DdR7RSVKkGs4E/giphy.gif'>"
   },
   {
       question: "What is the position that Harry Potter played on the Quidditch team?",
       answers: ['Beater', 'Keeper', 'Chaser', 'Seeker'],
-      correctAnswer: 'Seeker'
+      correctAnswer: 'Seeker',
+      image: "<img src='https://media.giphy.com/media/jABtk7ctFmPPq/giphy.gif'>"
   },
   {
       question: "What is Hermione Granger's Patronus?",
       answers: ['otter', 'horse', 'fox','rabbit'],
-      correctAnswer: 'otter'
+      correctAnswer: 'otter',
+      image: "<img src='https://media.giphy.com/media/O1lbWlwZilQo8/giphy.gif'>"
   },
   {
       question: "What color are Dobby the House Elf's eyes?",
       answers: ['Brown', 'Green', 'Blue', 'Black'],
-      correctAnswer: 'Green'
+      correctAnswer: 'Green',
+      image: "<img src='https://media.giphy.com/media/RaTAFdOkxs0X6/giphy.gif'>"
   },
   {
       question: "What spell does Gilderoy Lockhart accidentally cast on himself in Harry Potter and the Chamber of Secrets?",
       answers: ['Sectumsempra', 'Imperio','Expelliarmus', 'Obliviate'],
-      correctAnswer: 'Obliviate'
+      correctAnswer: 'Obliviate',
+      image: "<img src='https://media.giphy.com/media/lffThFKTbC0hpf4KfP/giphy.gif'>"
   },
   {
       question: "What does Vernon Dursley's company sell?",
       answers: ['Drills', 'Cars', 'Software', 'Furniture'],
-      correctAnswer: 'Drills'
+      correctAnswer: 'Drills',
+      image: "<img src='https://media.giphy.com/media/iiLzra7xb7oze/giphy.gif'>"
   }
 
 ]
@@ -65,16 +74,15 @@ $(document).on('click','#reset', function (){
   game.reset();
 })
 var audioElement = document.createElement("audio");
-// attach to mp3 file
-audioElement.setAttribute("src", "./assests/images/harry-potter-dubstep-remix.mp3");
-// play audio button 
+audioElement.setAttribute("src", "./assests/images/harry-potter-dubstep-remix.mp3"); 
 $(".theme-button").on("click", function () {
     audioElement.play();
 });
-// pause audio button
 $(".pause-button").on("click", function () {
     audioElement.pause();
 });
+
+
 var game = {
   questions: questions,
   currentQuestion:0,
@@ -86,7 +94,6 @@ var game = {
     game.counter--;
     $('#counter').html(game.counter);
     if(game.counter<=0){
-      console.log('time up!');
       game.timeUp();
     }
   },
@@ -138,7 +145,7 @@ var game = {
     clearInterval(timer);
     game.correct++;
     $('#subwrapper').html("<h2>YOU GOT IT RIGHT!</h2>")
-    $('#subwrapper').append("<img src='https://media.giphy.com/media/8l1UFvNSS5SwM/giphy.gif'>")
+    $('#subwrapper').append(questions[this.currentQuestion].image)
     if(game.currentQuestion==questions.length-1){
       setTimeout(game.results,3*1000)
     } else {
@@ -149,7 +156,7 @@ var game = {
     clearInterval(timer);
     game.incorrect++;
     $('#subwrapper').html("<h2>YOU GOT IT WRONG!</h2><h3>The Correct Answer Was: " + questions[game.currentQuestion].correctAnswer+"</h3")
-    $('#subwrapper').append("<img src='https://media.giphy.com/media/720g7C1jz13wI/source.gif'></img>")
+    $('#subwrapper').append(questions[this.currentQuestion].image)
     if(game.currentQuestion==questions.length-1){
       setTimeout(game.results,3*1000)
     } else {
